dccb9f6d0ee453dd1bbd6532014f2da470249d6f
bereketdinku <bereketdinku80@gmail.com>
2024-04-18 06:46:58 -0700

    cors

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL="https://doctro-appointment-backend.onrender.com"
+export const BASE_URL="https://doctroappointment.onrender.com"
 export const token=localStorage.getItem('token')
